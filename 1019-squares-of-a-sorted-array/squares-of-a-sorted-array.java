35c74c87a3d85289bc469fe969c12c59a10ae61b
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-05-07 01:31:29 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (86.54%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,75 +1,19 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n=nums.length;
-        List <Integer> neg=new ArrayList<>();
-        List<Integer> pos=new ArrayList<>();
-        for(int num: nums){
-            if(num>=0){
-            pos.add(num);
-
-            }
-           else{ neg.add(num);
-        }}
-        if(neg.size()==0){
-            for(int i=0;i<n;i++){
-                nums[i]=nums[i]*nums[i];
-            }
-            return nums;
-
-        }
-        if(pos.size()==0){
-            for(int i=0;i<n;i++){
-            neg.set(i,neg.get(i)*neg.get(i));
-            }
-            Collections.reverse(neg);
-           int[] arr = new int[neg.size()];
-
-for (int i = 0; i < neg.size(); i++) {
-    arr[i] = neg.get(i);
-}
-return arr;
-
-        }
-        int i=0 ,j=0, id=0;
-        int n1=neg.size();
-        int n2=pos.size();
-        int []res=new int[n1+n2];
-
-        for(i=0;i<n1;i++){
-            neg.set(i,neg.get(i)*neg.get(i));
-        }
-        Collections.reverse(neg);
-        
-        for(j=0;j<n2;j++){
-            pos.set(j,pos.get(j)*pos.get(j));
-        }
-
-        i=0 ;j=0;
-        while(i<n1 && j<n2){
-            if(neg.get(i)<=pos.get(j)){
-                res[id]=neg.get(i);
-                i++;
-                id++;
-            }
-            else{
-                res[id]=pos.get(j);
-                id++;
-                j++;
+        int[] result = new int[nums.length];
+        int left = 0, right = nums.length-1, index = nums.length-1;
+        while (left <= right) {
+            int leftSquare = nums[left]*nums[left];
+            int rightSquare = nums[right]*nums[right];
+            if (leftSquare < rightSquare) {
+                result[index--] = rightSquare;
+                right--;
+            } else {
+                result[index--] = leftSquare;
+                left++;
             }
         }
-        while(i<n1){
-            res[id]=neg.get(i);
-            i++;
-            id++;
-        }
-        while(j<n2){
-            res[id]=pos.get(j);
-            j++;
-            id++;
-        }
-return res;
 
-
-        
+        return result;
     }
 }
