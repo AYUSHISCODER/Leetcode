630dbdfaea4c78d7c89e594a930976045c95a0dc
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-06-12 12:50:16 +0530

    Time: 28 ms (72.61%), Space: 57 MB (27.84%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,46 +1,43 @@
 class Solution {
-    class pair{
+    class pair {
         int index;
         int second;
 
-        pair(int id,int dis){
-            this.index=id;
-            this.second=dis;
+        pair(int id, int dis) {
+            this.index = id;
+            this.second = dis;
         }
     }
+
     public int[][] kClosest(int[][] points, int k) {
-        int n=points.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        PriorityQueue<pair> pq=new PriorityQueue<>(
-            (a,b)->{ 
-                return b.second-a.second;
-            }
+
+        PriorityQueue<pair> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(b.second, a.second)
         );
-        for(int i=0;i<n;i++){
-            int d=(points[i][0]*points[i][0]) + (points[i][1]*points[i][1]);
-            map.put(i,d);
+
+        for (int i = 0; i < points.length; i++) {
+
+            int dist = points[i][0] * points[i][0]
+                     + points[i][1] * points[i][1];
+
+            if (pq.size() < k) {
+                pq.offer(new pair(i, dist));
+            } else if (dist < pq.peek().second) {
+                pq.poll();
+                pq.offer(new pair(i, dist));
+            }
         }
-        for(int key:map.keySet()){
-            if(pq.size()<k){
-                pq.add(new pair(key,map.get(key)));
-            }
-            else{
-                if(map.get(key)<pq.peek().second){
-                    pq.poll();
-                    pq.add(new pair(key,map.get(key)));
-                }
-            }
- }
-int idx=0;
-int ans[][]=new int[k][2];
-while(!pq.isEmpty()){
-    int i=pq.poll().index;
-    ans[idx][0]=points[i][0];
-    ans[idx][1]=points[i][1];
-    idx++;
-}
 
- return ans;
+        int[][] ans = new int[k][2];
+        int idx = 0;
 
+        while (!pq.isEmpty()) {
+            int i = pq.poll().index;
+            ans[idx][0] = points[i][0];
+            ans[idx][1] = points[i][1];
+            idx++;
+        }
+
+        return ans;
     }
 }
