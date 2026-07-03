9c6258a5fe940372297de0396d58de44e7783509
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-07-04 00:12:56 +0530

    Time: 2 ms (17.57%), Space: 46.8 MB (12.97%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -17,7 +17,7 @@ class Solution {
     public void recoverTree(TreeNode root) {
         fun(root);
         int n=list.size();
-       Collections.sort(list);
+      sort(list);
        replace(root);
     }
     ArrayList<Integer>list=new ArrayList<>();
@@ -39,5 +39,39 @@ class Solution {
       root.val=list.get(i++);
       replace(root.right);
     }
+    public void sort(ArrayList<Integer>list){
+        int wrong=0;
+        int wrong1=-1,wrong2=-1,wrong3=-1,wrong4=-1;
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i)>list.get(i+1)){
+                if(wrong==0){
+                    wrong1=i;
+                    wrong2=i+1;
+                    wrong++;
+                }
+                else{
+                    wrong3=i;
+                    wrong4=i+1;
+                    wrong++;
+                }
+            }
+        }
+        if(wrong==1){
+            swap(list,wrong1,wrong2);
+            
+        }
+        else{
+            swap(list ,wrong1,wrong4);
+        }
+        
+    }
+     public void swap(ArrayList<Integer> list, int i, int j) {
+        int temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+    
+
+
 
 }
