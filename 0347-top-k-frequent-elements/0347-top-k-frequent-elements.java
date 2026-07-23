9fd7b1f0635e72dd7135ffd9269f137c005e2528
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-07-23 10:22:57 +0530

    Time: 13 ms (79.94%), Space: 48 MB (12.6%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,41 +1,39 @@
-//O(nlog(n))  try better
-
 class Solution {
-   class pair{ int first;
-    int second;
-    pair (int f,int s){
-        this.first=f;
-        this.second=s;
-    }}
+    class pair{
+        int first;
+        int second;
+        pair(int first,int second){
+           this.first= first;
+           this.second=second;
+        }
+    }
+
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
         PriorityQueue<pair> pq=new PriorityQueue<>(
             (a,b)->{
-                
-            
-                    return a.second-b.second;
-                
+                return a.second-b.second;
             }
         );
-       for(int x:nums){
-        map.put(x,map.getOrDefault(x,0)+1);
-       }
-       int count=0;
-       for(int key : map.keySet()){
-        if(pq.size()<k){
-        pq.add(new pair(key,map.get(key)));
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        else{
-             if(pq.peek().second<map.get(key)){
-          pq.poll();
-          pq.add(new pair(key,map.get(key)));
+        for(int key: map.keySet()){
+            if(pq.size()<k){
+                pq.add(new pair(key,map.get(key)));
+            }
+            else{
+                if(map.get(key)>pq.peek().second){
+                    pq.poll();
+                    pq.add(new pair(key,map.get(key)));
+                }
+            }
         }
-          }  }
-       int ans[]=new int[k];
-       for(int i=0;i<k;i++){
-       ans[i]=pq.poll().first;
-       }
+        int ans[]=new int[k];
+        for(int i=0;i<k;i++){
+            ans[i]=pq.poll().first;
+        }
+        return ans;
 
-return ans;
     }
-    }
+}
