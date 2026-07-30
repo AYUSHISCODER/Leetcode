971993159d9d2f0ce146420d8c076b457377723f
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-07-30 15:01:06 +0530

    Time: 2 ms (96.39%), Space: 45.6 MB (83.48%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>  ans=new ArrayList<>();
+        List<Integer> help=new ArrayList<>();
+        int i=0;
         int n=candidates.length;
-         List<Integer> sub= new ArrayList<>();
-         List<List<Integer>> ans=new ArrayList<>();
-         answer(candidates,sub,ans,0,target,n);
+
+         fun(ans,help,candidates,i,n,target);
          return ans;
-        
     }
-    public static void answer(int[]candidates,List<Integer>sub,List<List<Integer>>ans,int id,int target,int n){
-
-        if(id==n){
-            if(target==0){
-                ans.add(new ArrayList<>(sub));
-            }
-            return;
-        }  
-        
-
-        if(candidates[id]<=target){
-            sub.add(candidates[id]);
-            answer(candidates,sub,ans,id,target-candidates[id],n);
-            sub.remove(sub.size()-1);
+    public void fun( List<List<Integer>>  ans, List<Integer> help,int[] candidates,int index,int n,int target){
+        if(target==0){
+            ans.add(new ArrayList<>(help));
+            return ;
         }
-          answer(candidates,sub,ans,id+1,target,n);
+        if(index==n|| target<0){
+            return ;
+        }
+        
+        help.add(candidates[index]);
+       fun(ans,help,candidates,index,n,target-candidates[index]);
+        help.remove(help.size()-1);
+        fun(ans,help,candidates,index+1,n,target);
 
     }
 }
