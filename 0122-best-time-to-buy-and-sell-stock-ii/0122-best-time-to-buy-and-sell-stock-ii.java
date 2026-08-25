82dd1e49de63c15dd0709ec2f99aa1ffd6b39770
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-08-26 00:13:17 +0530

    Time: 2 ms (16.99%), Space: 47.9 MB (10.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,32 +1,33 @@
 class Solution {
-    Integer dp[][];
     public int maxProfit(int[] prices) {
-     int i=0;
-     int k=2;
-     int n=prices.length;
-     dp=new Integer[n+1][k+1];
-     return fun(prices,i,n,k);  
-    }
-    public int fun(int []prices,int i,int n,int k){
-        if(i==n){
-            return 0;
+        int n = prices.length;
+        int k = 2;
+
+        int dp[][] = new int[n + 1][k + 1];
+
+  
+        for (int j = 0; j <= k; j++) {
+            dp[n][j] = 0;
         }
-        if(k==0){
-            return 0;
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
         }
-        if(dp[i][k]!=null){
-            return dp[i][k];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            dp[i][k] = Math.max(
+                dp[i + 1][k - 1] - prices[i],
+                dp[i + 1][k]
+            );
+
+     
+            dp[i][k - 1] = Math.max(
+                dp[i + 1][k] + prices[i],
+                dp[i + 1][k - 1]
+            );
         }
-        if(k==2){
-            int c1=fun(prices,i+1,n,k-1) - prices[i];
-            int c2=fun(prices,i+1,n,k);
-            return dp[i][2]= Math.max(c1,c2);
-        }
-        else{
-            int c1=fun(prices,i+1,n,k+1)+prices[i];
-            int c2=fun(prices,i+1,n,k);
-            return  dp[i][1]=Math.max(c1,c2);
-              
-        }
+
+        return dp[0][k];
     }
 }
