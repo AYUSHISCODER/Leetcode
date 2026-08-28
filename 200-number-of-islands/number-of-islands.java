b6094b203abab4754ca02f45f5f42f12837ed0c5
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-08-29 00:50:50 +0530

    Time: 4 ms (46.56%) | Memory: 52.3 MB (58.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,37 +1,39 @@
 class Solution {
-    int[] x = {-1,1,0, 0};
-    int[] y = {0,0,-1, 1};
-
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        int count = 0;
-
-        for (int i = 0; i < m; i++) {
-     for (int j = 0; j < n; j++) {
-                if(grid[i][j] =='1' && !visited[i][j]) {
-                    count++;
-                    dfs(grid, visited, i, j);
+        // int i=0;
+        // int j=0;
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean vis[][]=new boolean[m][n];
+        int res=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && vis[i][j]==false){
+                    dfs(grid,i,j,vis,m,n);
+                    res++;
                 }
             }
         }
-
-        return count;
+        return res;
     }
-public void dfs(char[][] grid, boolean[][] visited, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
-            return;
-            if (grid[i][j] == '0' || visited[i][j])
-            return;
-
-        visited[i][j] = true;
-
-        for (int k = 0; k < 4; k++) {
-            int ni = i + x[k];
-            int nj = j + y[k];
-            dfs(grid, visited, ni, nj);
+    public boolean valid (int i,int j,int m,int n){
+        if(i<0 ||i>=m||j<0||j>=n){
+            return false;
         }
+        return true;
+    }
+    int x[]={-1,1,0,0};
+    int y[]={0,0,-1,1};
+    public void dfs(char[][] grid,int i,int j,   boolean vis[][],int m ,int n){
+        vis[i][j]=true;
+        for(int k=0;k<4;k++){
+            int row=i+x[k];
+            int col=j+y[k];
+            if(valid(row,col,m,n)&& grid[row][col]=='1' && vis[row][col]==false){
+                dfs(grid,row,col,vis,m,n);
+            }
+          
+        }
+          return ;
     }
 }
