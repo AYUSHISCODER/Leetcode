b00f49d6c5409bd4d0b8d2389fab89114f0585c8
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-08-31 23:15:16 +0530

    Time: 1 ms (73.24%) | Memory: 45.1 MB (54.20%) - LeetSync

--- 2386-min-max-game/min-max-game.java
+++ 2386-min-max-game/min-max-game.java
@@ -1,27 +1,22 @@
 class Solution {
     public int minMaxGame(int[] nums) {
-        ArrayList<Integer>ans=new ArrayList<>();
-        for(int x :nums){
-            ans.add(x);
-        }
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        int flag=0;
-        while(ans.size()>1){
-            
-            for(int i=0;i<ans.size();i++){
-              if(flag==0){
-                ans.set(i,Math.min(ans.get(i),ans.get(i+1)));
-                ans.remove(i+1);
-                flag=1;
-              }
-              else if(flag==1){
-                ans.set(i,Math.max(ans.get(i),ans.get(i+1)));
-                ans.remove(i+1);
-                flag=0;
-              }
+        int n = nums.length;
+
+        while (n > 1) {
+            int[] next = new int[n / 2];
+
+            for (int i = 0; i < n / 2; i++) {
+                if (i % 2 == 0) {
+                    next[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+                } else {
+                    next[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+                }
             }
+
+            nums = next;
+            n = n / 2;
         }
-        return ans.get(0);
+
+        return nums[0];
     }
 }
