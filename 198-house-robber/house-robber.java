6ad9ddeaf01caeb6c1dc776138944359f66bbc03
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-09-07 15:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (5.78%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,22 @@
 class Solution {
     Integer dp[][];
     public int rob(int[] nums) {
-        int free=1;
         int n=nums.length;
-        dp=new Integer[n][2];
-        return fun(nums,n,0,free);
+        int i=0;
+        int free=1;
+        dp=new Integer[n+1][free+1];
+    
+        return fun(nums,n,i,free);
     }
     public int fun(int[]nums,int n,int i,int free){
         if(i==n){
             return 0;
         }
-         if (dp[i][free] != null)
-            return dp[i][free];
         if(free==0){
-            return dp[i][free]= fun(nums,n,i+1,1);
+       return  fun(nums,n,i+1,1);
+        }
+        if(dp[i][free]!=null){
+            return dp[i][free];
         }
         int c1=nums[i]+fun(nums,n,i+1,0);
         int c2=fun(nums,n,i+1,1);
