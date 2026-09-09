bf09c918983daa5fdd58b5f76b22883c49ab62da
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-09-09 12:05:37 +0530

    Time: 22 ms (93.35%), Space: 44.5 MB (66.5%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,21 +1,29 @@
 class Solution {
-    Integer dp[];
+   // Integer dp[];
     public int numSquares(int n) {
-    dp=new Integer[n+1];
-    return solve(n);
+  int[]  dp=new int[n+1];
+  dp[0]=0;
+  for(int i=1;i<=n;i++){
+  int ans=Integer.MAX_VALUE;
+  for(int j=1;j*j<=i;j++){
+ans =Math.min(ans,1+dp[i-j*j]);
+  }
+  dp[i]=ans;
+  }
+  return dp[n];
       
     }
-    public  int solve(int n){
-      if(n==0){
-            return 0;
-        }
-        if(dp[n]!=null){
-            return dp[n];
-        }
-        int ans=Integer.MAX_VALUE;
-        for(int i=1;i*i<=n;i++){
-         ans=  Math.min(ans,1+solve(n-i*i));
-        }
-  return dp[n]= ans;
-    }
+//     public  int solve(int n){
+//       if(n==0){
+//             return 0;
+//         }
+//         if(dp[n]!=null){
+//             return dp[n];
+//         }
+//         int ans=Integer.MAX_VALUE;
+//         for(int i=1;i*i<=n;i++){
+//          ans=  Math.min(ans,1+solve(n-i*i));
+//         }
+//   return dp[n]= ans;
+//     }
 }
