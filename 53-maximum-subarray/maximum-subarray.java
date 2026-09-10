12a34fe1a6bc9d11a1e1aea87db5244f52501db2
AYUSHISCODER <ayushkumarnayak197@gmail.com>
2026-09-10 11:13:54 +0530

    Time: 1 ms (99.93%) | Memory: 77.3 MB (32.54%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -4,12 +4,13 @@ class Solution {
         int best=nums[0];
         int ans=nums[0];
         for(int i=1;i<n;i++){
-            int v1= best + nums[i];
+            int v1=best+nums[i];
             int v2=nums[i];
             best=Math.max(v1,v2);
-            ans=Math.max(best,ans);
+            ans=Math.max(ans,best);
+            
         }
         return ans;
-        
+
     }
 }
